d5b3d2ed15327791775d6b18b07bd6bc5e974f90
Brian Kim <brian.kim.2011@gmail.com>
2024-11-13 13:22:09 -0800

    Add calls to cli example

--- example/cli/main_cli.sh
+++ example/cli/main_cli.sh
@@ -1,1 +1,19 @@
-# TODO
+#!/bin/bash
+
+fwirl summarize test_graph
+
+fwirl ls test_graph --assets --schedules --jobs
+
+fwirl build test_graph
+
+fwirl schedule test_graph test_build_schedule build '5 * * * *'
+
+fwirl pause test_graph Reliable2
+
+fwirl refresh test_graph
+
+fwirl unpause test_graph Reliable2
+
+fwirl unschedule test_graph test_build_schedule
+
+fwirl shutdown test_graph
