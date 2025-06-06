4e3df35e329a95386e53bfa8ebea661e8e260b19
Brian Kim <brian.kim.2011@gmail.com>
2025-06-06 12:49:44 -0700

    Add lines pausing and unpausing schedule

--- example/cli/main_cli.sh
+++ example/cli/main_cli.sh
@@ -8,11 +8,19 @@ fwirl build test_graph
 
 fwirl schedule test_graph test_build_schedule build '5 * * * *'
 
-fwirl pause test_graph Reliable2
+fwirl pause test_graph Reliable
 
 fwirl refresh test_graph
 
-fwirl unpause test_graph Reliable2
+fwirl unpause test_graph Reliable
+
+fwirl pause test_graph test_build_schedule
+
+fwirl ls test_graph --schedules --jobs
+
+fwirl unpause test_graph test_build_schedule
+
+fwirl ls test_graph --schedules --jobs
 
 fwirl unschedule test_graph test_build_schedule
 
